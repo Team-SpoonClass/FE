ef408b75e6cf2b10fb2e75d2dcca7e59e0a7f641
HamInGyu <hfire1207@gmail.com>
2021-12-01 14:13:09 +0900

    🔥[FEAT] : 메인페이지 서버연동 완료

--- app/src/lib/client.js
+++ app/src/lib/client.js
@@ -5,6 +5,6 @@ const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const client = axios.create();
 
 client.defaults.baseURL = BACKEND_URL;
-client.defaults.withCredentials = true;
+//client.defaults.withCredentials = true;
 
 export default client;
--- app/src/pages/MainPage/index.js
+++ app/src/pages/MainPage/index.js
@@ -1,60 +1,31 @@
 import "./index.css";
 import ClassCard from "components/ClassCard";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-
-const classList = [
-  {
-    id: 1,
-    name: "일이삼사오육칠팔구십일이삼사오육칠팔구십일이삽사오육칠팔구십",
-    oneLineInfo:
-      "일이삼사오육칠팔구십일이삼사오육칠팔구십일이삽사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삽사오육칠팔구십",
-    club: "동아리 이름1",
-  },
-  {
-    id: 2,
-    name: "클래스 이름2",
-    oneLineInfo: "한줄 소개 블라블라",
-    club: "동아리 이름2",
-  },
-  {
-    id: 3,
-    name: "클래스 이름3",
-    oneLineInfo: "한줄 소개 블라블라",
-    club: "동아리 이름3",
-  },
-  {
-    id: 4,
-    name: "클래스 이름4",
-    oneLineInfo: "한줄 소개 블라블라",
-    club: "동아리 이름4",
-  },
-  {
-    id: 5,
-    name: "클래스 이름5",
-    oneLineInfo: "한줄 소개 블라블라",
-    club: "동아리 이름5",
-  },
-  {
-    id: 6,
-    name: "클래스 이름6",
-    oneLineInfo: "한줄 소개 블라블라",
-    club: "동아리 이름6",
-  },
-  {
-    id: 7,
-    name: "클래스 이름7",
-    oneLineInfo: "한줄 소개 블라블라",
-    club: "동아리 이름7",
-  },
-];
+import { useEffect, useState } from "react";
+import client from "lib/client";
 
 function MainPage({ userObj }) {
   const navigate = useNavigate();
+  const [classList, setClassList] = useState([]);
+
+  const loadClassList = async () => {
+    client.defaults.headers.common["x-auth-token"] = userObj.oriToken;
+    try {
+      await client.get("/lesson").then((res) => {
+        if (res.status === 200) {
+          setClassList(() => res.data.lessonDetailDto);
+        }
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
 
   useEffect(() => {
     if (!userObj) {
       navigate("/landing");
+    } else {
+      loadClassList();
     }
   }, [userObj, navigate]);
   return (
