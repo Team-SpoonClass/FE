55f259aa84ed99c6138f2e24d184642ab0b84f79
HamInGyu <hfire1207@gmail.com>
2021-12-01 15:16:07 +0900

    🔥[FEAT] : 클래스 생성하고 리다이렉트

--- app/src/components/ClassForm/index.js
+++ app/src/components/ClassForm/index.js
@@ -2,13 +2,16 @@ import "./index.css";
 import { ClassInput, ClassTextarea } from "components/Input";
 import { CustomSubmitBtn } from "components/CustomButton";
 import useInput from "hooks/useInput";
+import { useNavigate } from "react-router-dom";
+import client from "lib/client";
 
-function ClassForm({ purpose }) {
-  const name = useInput("");
-  const oneLineInfo = useInput("", (value) => value.length <= 50);
-  const club = useInput("");
-  const description = useInput("");
-  const openKakao = useInput("");
+function ClassForm({ purpose, classData }) {
+  const navigate = useNavigate();
+  let name = useInput("");
+  let oneLineInfo = useInput("", (value) => value.length <= 50);
+  let club = useInput("");
+  let description = useInput("");
+  let openKakao = useInput("");
 
   let buttonValue;
 
@@ -23,13 +26,37 @@ function ClassForm({ purpose }) {
       break;
   }
 
+  const createSubmit = async () => {
+    try {
+      console.log(client.defaults);
+      const reqBody = {
+        name: name.value,
+        oneLineInfo: oneLineInfo.value,
+        club: club.value,
+        description: description.value,
+        openKakao: openKakao.value,
+      };
+      await client.post("/lesson/create", reqBody).then((res) => {
+        if (res.status === 200) {
+          navigate(`/class/${res.data.id}`);
+        }
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
   const onSubmit = async (event) => {
     event.preventDefault();
-    console.log(name.value);
-    console.log(oneLineInfo.value);
-    console.log(club.value);
-    console.log(description.value);
-    console.log(openKakao.value);
+    switch (purpose) {
+      case "new":
+        createSubmit();
+        break;
+      case "update":
+        buttonValue = "클래스 수정하기";
+        break;
+      default:
+        break;
+    }
   };
 
   return (
--- app/src/pages/ClassCreatePage/index.js
+++ app/src/pages/ClassCreatePage/index.js
@@ -1,7 +1,16 @@
 import "./index.css";
 import ClassForm from "components/ClassForm";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-function ClassCreatePage() {
+function ClassCreatePage({ userObj }) {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!userObj) {
+      navigate("/landing");
+    }
+  }, [userObj, navigate]);
+
   return (
     <main className="create-class">
       <div className="inner">
@@ -9,7 +18,7 @@ function ClassCreatePage() {
           <h1 className="page-title">새 클래스 만들기</h1>
         </div>
         <div className="inner__second">
-          <ClassForm purpose="new" />
+          <ClassForm purpose="new" classData={null} />
         </div>
       </div>
     </main>
